a2c87fbbe99f7d69a89ac7de77374877fc639998
marshmellow2178 <115971843+marshmellow2178@users.noreply.github.com>
2024-02-17 12:36:39 +0900

    .

--- src/main/java/com/marshmellow/myboard/member/MemberRepo.java
+++
@@ -1,9 +0,0 @@
-package com.marshmellow.myboard.member;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepo extends JpaRepository<Member, Long> {
-	Optional<Member> findByUid(String uid);
-}
